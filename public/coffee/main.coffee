bd5e45fcd451cd6c3694f16708fd65c721fd835f
Chris Dahms <zentrification@gmail.com>
2013-11-14 11:43:56 -0500

    centered content properly

--- public/coffee/main.coffee
+++ public/coffee/main.coffee
@@ -12,10 +12,13 @@ $.fn.fullpage
     'chapter9'
   ]
   menu: '#menu'
+  paddingTop: '60'
+  resize: false
   scrollOverflow: true
+  verticalCentered: false
 
 $ ->
   $('#intro-arrow').fadeIn(4000)
   window_height = $(window).height()
-  for div in $('div.content')
+  for div in $('div.content, div.content-centerer')
     $(div).css('height', "#{window_height}px")
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -2,14 +2,17 @@
   $.fn.fullpage({
     anchors: ['chapter0', 'chapter1', 'chapter2', 'chapter3', 'chapter4', 'chapter5', 'chapter6', 'chapter7', 'chapter8', 'chapter9'],
     menu: '#menu',
-    scrollOverflow: true
+    paddingTop: '60',
+    resize: false,
+    scrollOverflow: true,
+    verticalCentered: false
   });
 
   $(function() {
     var div, window_height, _i, _len, _ref, _results;
     $('#intro-arrow').fadeIn(4000);
     window_height = $(window).height();
-    _ref = $('div.content');
+    _ref = $('div.content, div.content-centerer');
     _results = [];
     for (_i = 0, _len = _ref.length; _i < _len; _i++) {
       div = _ref[_i];
