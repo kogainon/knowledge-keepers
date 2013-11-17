e374d6674530b4021c2baefd318dd26f994bb125
Chris Dahms <zentrification@gmail.com>
2013-11-17 15:48:38 -0500

    created environment for livereload

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 
 get '/*' do
   @cloudfront_url = 'http://d1vnylnhovobpx.cloudfront.net'
+  @environment = settings.environment || 'development'
   @request_url = request.host
   cache_control :public, max_age: 1
   slim :index
